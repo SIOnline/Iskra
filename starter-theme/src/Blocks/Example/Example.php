50b93769a8354429fa39b3c3305ea940d7a936cd
marcin-biernacki-sionline <80921366+marcin-biernacki-sionline@users.noreply.github.com>
2022-05-15 12:07:10 +0200

    cleanup

--- starter-theme/src/Blocks/Example/Example.php
+++ starter-theme/src/Blocks/Example/Example.php
@@ -22,27 +22,23 @@
                     'mode' => 'preview', //preview / edit / auto
                     'enqueue_assets' => function(){
                         $block_name = get_class($this);
-                        wp_enqueue_style( 
-                            "$block_name-css", 
-                            get_template_directory_uri() . "/src/Blocks/$block_name/_$block_name.css", 
-                            false,
-                            '1'
-                        );
+                        wp_enqueue_style( "$block_name-css", get_template_directory_uri() . "/src/Blocks/$block_name/_$block_name.css", false, '1' );
                         wp_enqueue_script( "$block_name-js", get_template_directory_uri() . "/src/Blocks/$block_name/$block_name.js", [], '', true );
                     },
                     //'supports'
                     'render_callback' => [$this, 'render']
             ]);
         }
+
+
+            
     }
 
     public function render() { 
         
         $acf = get_fields();
-        
         $title = $acf['title'] ?? false;
 
-        $c = get_class($this);
         ?>
 
         <section class="Example">
