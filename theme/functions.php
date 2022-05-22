20a9d2a71024e518b458b018c42e40a24c6a4a5c
marcin-biernacki-sionline <80921366+marcin-biernacki-sionline@users.noreply.github.com>
2022-05-22 05:19:32 +0200

    cleanup

--- theme/functions.php
+++ theme/functions.php
@@ -1,23 +1,21 @@
-<?php 
+<?php
 
 /**
- * gutenberg styles
+ * Gutenberg Include styles
  */
 
-function misha_gutenberg_css(){
-
+function iskra_gutenberg_css(){
 	add_theme_support( 'editor-styles' );
-	add_editor_style( 'style-editor.css' ); // tries to include style-editor.css directly from your theme folder
-
+	add_editor_style( 'build/Editor.css' );
 }
 
-add_action( 'after_setup_theme', 'misha_gutenberg_css' );
+add_action( 'after_setup_theme', 'iskra_gutenberg_css' );
 
 /**
- *  
+ * Gutenberg Custom categories
  */
 function filter_block_categories_when_post_provided( $block_categories, $editor_context ) {
-    
+
         $category_headings = array(
             'slug'  => 'header',
             'title' => __( 'Headers', 'starter-theme' ),
@@ -29,15 +27,15 @@ function filter_block_categories_when_post_provided( $block_categories, $editor_
             'title' => __( 'Sections', 'starter-theme' ),
             'icon' => null,
         );
-        
-    array_unshift( 
-        $block_categories, 
-        $category_headings, 
+
+    array_unshift(
+        $block_categories,
+        $category_headings,
         $category_sections
     );
     return $block_categories;
 }
- 
+
 add_filter( 'block_categories_all', 'filter_block_categories_when_post_provided', 1, 2 );
 
 /**
@@ -45,10 +43,9 @@ add_filter( 'block_categories_all', 'filter_block_categories_when_post_provided'
  */
 $blocks = [
     'Example',
-    'ExampleSection'
 ];
 
 foreach ($blocks as $block) {
     require_once "src/Blocks/$block/$block.php";
-    new $block(); 
+    new $block();
 }
